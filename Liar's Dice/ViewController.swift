28047e4ffb0ab02e417b6618d3f9f9357c180818
tparisotto <t.parisotto@student.rug.nl>
2020-02-14 12:41:24 +0100

    Initial Commit

---
+++ Liar's Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Liar's Dice
+//
+//  Created by Tommaso Parisotto on 14/02/2020.
+//  Copyright © 2020 CogMod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
