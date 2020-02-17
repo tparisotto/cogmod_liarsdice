eb079d1cf8d99964dcdcf5aa8cd41b1c0cf0e974
Andrei Miculita <andreimiculita@gmail.com>
2020-02-17 11:49:07 +0100

    Adding unpicked

--- Liar's Dice/ViewController.swift
+++ Liar's Dice/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // the number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        // number of columns
+        // could probably be 2 but I'm not sure I can mix text and images
+        return 1
+    }
+    
+    // the number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    @IBOutlet weak var numberOfDicePicker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Connect data:
+        self.numberOfDicePicker.delegate = self
+        self.numberOfDicePicker.dataSource = self
+        
+        pickerData = ["1", "2", "3", "4", "5", "6"]
+        
     }
 
 
