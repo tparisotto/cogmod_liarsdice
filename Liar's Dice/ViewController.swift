20d78c5ffabb51320a4d7f03b8cf78db4d4b366e
Andrei Miculita <andreimiculita@gmail.com>
2020-02-21 13:34:34 +0100

    Aligning storyboard, renaming some variables

--- Liar's Dice/ViewController.swift
+++ Liar's Dice/ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var typeOfDicePicker: UIPickerView!
     @IBOutlet weak var numberOfDicePicker: UIPickerView!
     @IBOutlet weak var betButton: UIButton!
-    @IBOutlet weak var playerHandView: UIImageView!
-    @IBOutlet weak var opponentHandView: UIImageView!
+    @IBOutlet weak var playerCupView: UIImageView!
+    @IBOutlet weak var opponentCupView: UIImageView!
     @IBOutlet weak var opponentBetTypeOfDiceView: UIImageView!
     @IBOutlet weak var opponentBetNumberOfDiceView: UILabel!
     
@@ -27,21 +27,17 @@ class ViewController: UIViewController {
         // Aesthetic stuff
         betButton.layer.cornerRadius = 5
         
-        // Normally the delegate and data source would be implemented in this controller but we need 2 of each
-        
-        print(typeOfDicePickerAdapter.pickerView(numberOfDicePicker, numberOfRowsInComponent: 10))
         // Connect data:
         self.typeOfDicePicker.delegate = typeOfDicePickerAdapter
         self.typeOfDicePicker.dataSource = typeOfDicePickerAdapter
         self.typeOfDicePicker.setValue(UIColor.white, forKeyPath: "textColor")
         
-        // Connect data:
         self.numberOfDicePicker.delegate = numberOfDicePickerAdapter
         self.numberOfDicePicker.dataSource = numberOfDicePickerAdapter
         self.numberOfDicePicker.setValue(UIColor.white, forKeyPath: "textColor")
         
         self.opponentBetNumberOfDiceView.text = "3"
-        self.opponentBetTypeOfDiceView = UIImageView(image: UIImage(named: "dice-six-faces-six")!)
+        self.opponentBetTypeOfDiceView.image = UIImage(named: "dice-six-faces-six")!
     }
 
 }
