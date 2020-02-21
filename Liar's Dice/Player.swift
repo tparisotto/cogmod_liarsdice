62c349b84447415f80fd6a9226a2fa1f1c86c98b
tparisotto <t.parisotto@student.rug.nl>
2020-02-21 12:07:35 +0100

    classes restored

---
+++ Liar's Dice/Player.swift
@@ -0,0 +1,46 @@
+//
+//  Player.swift
+//  Liar's Dice
+//
+//  Created by Tommaso Parisotto on 14/02/2020.
+//  Copyright © 2020 CogMod. All rights reserved.
+//
+
+import Foundation
+
+class Player
+{
+    let startingNumberOfDice = 6
+    var numberOfDiceInHand: Int
+    var hand: [Dice] = []
+    
+    init()
+    {
+        self.numberOfDiceInHand = self.startingNumberOfDice
+        for _ in 0..<startingNumberOfDice { self.hand.append(Dice()) }
+    }
+    
+    func loseDice()
+    {
+        if numberOfDiceInHand < 1
+        {
+            print("Error: attempting to remove dice with 0 dice in hand.")
+        }
+        else
+        {
+            self.hand.removeLast()
+            self.numberOfDiceInHand -= 1
+        }
+    }
+    
+    func addDice()
+    {
+        self.hand.append(Dice())
+        self.numberOfDiceInHand += 1
+    }
+    
+    func rollHand()
+    {
+        for index in 0..<numberOfDiceInHand { _ = self.hand[index].roll() }
+    }
+}
