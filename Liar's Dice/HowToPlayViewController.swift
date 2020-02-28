4d9a4931458f25df058c2b7c7163ff34738c8e33
Andrei Miculita <andreimiculita@gmail.com>
2020-02-28 12:44:29 +0100

    Adding back button to how to screen

---
+++ Liar's Dice/HowToPlayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Liar's Dice
+//
+//  Created by Tommaso Parisotto on 14/02/2020.
+//  Copyright © 2020 CogMod. All rights reserved.
+//
+
+import UIKit
+
+// color scheme
+// green       006600
+// light brown 99621E
+// dark brown  2F1000
+
+class HowToPlayViewController: UIViewController {
+    
+    
+    @IBAction func backButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+
+}
